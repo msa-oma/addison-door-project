31ccd9b6efb5dd7d24ffc82cc2471c762fce59bc
mohammed Sh A <62896318+msa-oma@users.noreply.github.com>
2023-03-09 17:33:29 +0200

    9-3-2023

--- Air-shower-test/employees-database.js
+++ Air-shower-test/employees-database.js
@@ -35,21 +35,24 @@ class EmployeeStatus {
     }
   }
 }
-var myemployee = new EmployeeStatus();
-myemployee.barcode = 4042;
-myemployee.IsOutside = true;
-myemployee.TimeStamp_Out = 123;
-myemployee.TimeStamp_In = 123;
-myemployee.Times = 2;
-myemployee.TolatTimeSpentOut = 5;
-myemployee.out_stamps = [123, 123, 123];
-myemployee.in_stamps = [321, 321, 321];
+//var myemployee = new EmployeeStatus();
+var temp_employee = new EmployeeStatus();
+temp_employee.barcode = 5555;
+temp_employee.IsOutside = false;
+temp_employee.TimeStamp_Out = 000;
+temp_employee.TimeStamp_In = 000;
+temp_employee.Times = 3;
+temp_employee.TolatTimeSpentOut = 1;
+temp_employee.out_stamps = [000, 000, 000];
+temp_employee.in_stamps = [000, 000];
 
 
-
-function set_object(myemployee) {
-  var temp_employee = new EmployeeStatus();
-  temp_employee = myemployee;
+////////////////////*******************************///////////////////////
+//insert Employee Status Object into database in table "employee-status"//
+//////////////////////////////////////////////////////////////////////////
+function insertEpmStatusInDB(temp_employee) {
+  // myemployee=MyEmployee;
+  //temp_employee = new EmployeeStatus();
 
 
   barcodeX = temp_employee.barcode;
@@ -58,18 +61,115 @@ function set_object(myemployee) {
   TimeStamp_InX = temp_employee.TimeStamp_In;
   TimesX = temp_employee.Times;
   TolatTimeSpentOutX = temp_employee.TolatTimeSpentOut;
-
-  // var toJson = JSON.stringify(temp_employee);
-  // jsonObj = JSON.parse(toJson);
-
-  const queryx = 'INSERT INTO `employee-status` (`barcode`, `Timestamp Out`, `Timestamp In`, `Total Time Spent Out`, `Is Outside`, `Times`,`out stamps`,`in stamps`, `date`) \
-   VALUES (?,?,?,?,?,?,?,?,current_timestamp())';
   const out_stamps_x = temp_employee.out_stamps.toString();
   const in_stamps_x = temp_employee.in_stamps.toString();
 
+  const insert_queryx = 'INSERT INTO `employee-status`\
+   (`barcode`, `Timestamp_Out`, `Timestamp_In`, \
+   `Total_Time_Spent_Out`, `Is_Outside`, `Times`,\
+   `out_stamps`,`in_stamps`, `date`) \
+   VALUES (?,?,?,?,?,?,?,?,current_timestamp())';
+
+  const del_queryx = 'DELETE FROM `employee-status` WHERE barcode= (?) \
+                                            AND date =CURDATE()';
+
+  const check_queryx = "SELECT * FROM `employee-status` WHERE barcode= (?) AND date =CURDATE() "
+   
+
+const update_queryx="UPDATE `employee-status` SET Timestamp_Out= (?),Timestamp_In= (?),\
+ Total_Time_Spent_Out= (?),Is_Outside= (?),Times= (?), out_stamps= (?),in_stamps= (?) \
+  WHERE  barcode= (?) AND date =CURDATE()"
+
   con.connect(function (err) {
     if (err) throw err;
+    console.log("Connected!");
+  });
 
+  con.query(check_queryx, [barcodeX], function (err, result, fields) {
+    if (err) throw err;
+    let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+    resultx.forEach((v, k) => { console.log(v); });
+
+    if (resultx.length == 1) {   //employee Exist in database 
+
+      console.log('EMPLOYEE ALREADY EXIST :)')
+      console.log("RESULT  >>> " + resultx.values());
+
+      console.log("RESULT LENGTH >>> " + resultx.length);
+
+      // con.query(del_queryx,
+      //   [barcodeX]
+      //   , function (err, result, fields) {
+      //     if (err) throw err;
+      //     let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+      //     resultx.forEach((v) => console.log(v));
+      //   });
+
+      con.query(update_queryx,
+        [TimeStamp_OutX,TimeStamp_InX,TolatTimeSpentOutX,IsOutsideX,TimesX,out_stamps_x,in_stamps_x,barcodeX]
+        , function (err, result, fields) {
+          if (err) throw err;
+          let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+          resultx.forEach((v) => console.log(v));
+          console.log('EMPLOYEE UPDATED :)')
+
+        });
+
+
+      /////
+      
+    }
+    else 
+    {
+      //console.log('EMPLOYEE NOT EXIST :(')
+      //Employee not exist
+      // so insert new record "employee object"
+      con.query(insert_queryx,
+        [barcodeX, TimeStamp_OutX,
+          TimeStamp_InX, TolatTimeSpentOutX,
+          IsOutsideX, TimesX, out_stamps_x, in_stamps_x]
+        , function (err, result, fields) {
+          if (err) throw err;
+          let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+          //resultx.forEach((v) => console.log(v));
+        });
+      console.log('EMPLOYEE ADDED :)')
+    }
+  });
+
+}
+//////////////////////////////////// * * * ////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+// insetr object into data base >>employee status table
+function set_object(myemployee) {
+  var temp_employee = new EmployeeStatus();
+  temp_employee = myemployee;
+
+  barcodeX = temp_employee.barcode;
+  IsOutsideX = temp_employee.IsOutside;
+  TimeStamp_OutX = temp_employee.TimeStamp_Out;
+  TimeStamp_InX = temp_employee.TimeStamp_In;
+  TimesX = temp_employee.Times;
+  TolatTimeSpentOutX = temp_employee.TolatTimeSpentOut;
+  const out_stamps_x = temp_employee.out_stamps.toString();
+  const in_stamps_x = temp_employee.in_stamps.toString();
+  // var toJson = JSON.stringify(temp_employee);
+  // jsonObj = JSON.parse(toJson);
+
+  // insert record into database in employee status table
+  const queryx = 'INSERT INTO `employee-status` (`barcode`, `Timestamp_Out`, `Timestamp_In`, `Total_Time_Spent_Out`, `Is_Outside`, `Times`,`out_stamps`,`in_stamps`, `date`) \
+   VALUES (?,?,?,?,?,?,?,?,current_timestamp())';
+  con.connect(function (err) {
+    if (err) throw err;
     con.query(queryx,
       [barcodeX, TimeStamp_OutX,
         TimeStamp_InX, TolatTimeSpentOutX,
@@ -81,15 +181,16 @@ function set_object(myemployee) {
         resultx.forEach((v) => console.log(v));
       });
   });
-
   console.log("here you go..");
 
 }
-// con.connect(function(err) {
+
+// insert into database in table employee-log
+// con.connect(function (err) {
 //   if (err) throw err;
 //   console.log("Connected!");
 //   var sql =
-//   "INSERT INTO `employees-log` (`barcode`, `timestamp`, `in or out`, `date`) VALUES (1058, 100000, 'in', current_timestamp())";
+//     "INSERT INTO `employees-log` (`barcode`, `timestamp`, `in or out`, `date`) VALUES (1058, 100000, 'in', current_timestamp())";
 //   con.query(sql, function (err, result) {
 //     if (err) throw err;
 //     console.log("1 record inserted");
@@ -108,34 +209,33 @@ function set_object(myemployee) {
 //     });
 // });
 
-/*
-  // insert record into database 'when entering hall
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = 
-  "INSERT INTO `employees-log` (`barcode`, `timestamp`, `in or out`, `date`) VALUES (barcode, EmployeeStatus(barcode).TimeStamp_In, 'in', current_timestamp())";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
-});
-  
+
+// insert record into database 'when entering hall in "EMPLOYEE-LOG" table 
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql =
+//     "INSERT INTO `employees-log` (`barcode`, `timestamp`, `in or out`, `date`) VALUES (barcode, EmployeeStatus(barcode).TimeStamp_In, 'in', current_timestamp())";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
+
+// insert record into database 'When exit the hall  in  "EMPLOYEE-LOG" table 
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql =
+//     "INSERT INTO `employees-log` (`barcode`, `timestamp`, `in or out`, `date`) VALUES (barcode, EmployeeStatus(barcode).TimeStamp_Out, 'out', current_timestamp())";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
 
 
-  // insert record into database 'When exit the hall
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = 
-    "INSERT INTO `employees-log` (`barcode`, `timestamp`, `in or out`, `date`) VALUES (barcode, EmployeeStatus(barcode).TimeStamp_Out, 'out', current_timestamp())";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-    });
-  });
-*/
-// select records from database
+// select all records from database in current day  >> "EMPLOYEE-LOG" 
 // con.connect(function (err) {
 //   if (err) throw err;
 //   con.query("SELECT * FROM `employees-log`WHERE date=CURDATE()  ", function (err, result, fields) {
@@ -146,118 +246,125 @@ con.connect(function(err) {
 //   });
 // });
 
-//
+//////////////////////////// / related to / EMPLOYEE-STATUS TABLE /////////////////////////////////
+// function checkIfExist(myemployee) {
+//   // get the query result and print it 
+//   //SElECT * FROM `employee-status` WHERE barcode = 'myemployee.barcode' AND date=CURDATE()
+//   //if(myemployee.barcode==result){continue...}
+//   // the select query must have barcode and currentdate conditions
+//   // look at this SELECT count(*) FROM commands WHERE barcode = 'myemployee.barcode' FOR EXAMPLE
+//   var x = new EmployeeStatus();
+//   x = myemployee;
 
-////////////////////////////// EMPLOYEE-STATUS TABLE /////////////////////////////////
-
-//INSERT INTO `employee-status` (`barcode`, `Timestamp Out`, `Timestamp In`, `Total Time Spent Out`, `Is Outside`, `Times`, `date`)
-// VALUES ('1020', '4315325', '52323525', '123', '0', '2', current_timestamp());
-
-////
-//   SELECT * FROM `employee-status`WHERE date=CURDATE()
-
-// con.connect(function (err) {
-//   if (err) throw err;
-
-//   con.query("SELECT * FROM `employee-status` WHERE date=CURDATE()  ", function (err, result, fields) {
+//   con.connect(function (err) {
 //     if (err) throw err;
-//     console.log(result);
-//     let resultx = Object.values(JSON.parse(JSON.stringify(result)));
-//     resultx.forEach((v) => console.log(v));
+//     // work on this one
+//     barcodeX = x.barcode;
+//     IsOutsideX = x.IsOutside;
+//     TimeStamp_OutX = x.TimeStamp_Out;
+//     TimeStamp_InX = x.TimeStamp_In;
+//     TimesX = x.Times;
+//     TolatTimeSpentOutX = x.TolatTimeSpentOut;
+//     const out_stamps_x = x.out_stamps.toString();
+//     const in_stamps_x = x.in_stamps.toString();
+
+//     const insert_queryx = 'INSERT INTO `employee-status` \
+//   (`barcode`, `Timestamp_Out`, `Timestamp_In`, `Total_Time_Spent_Out`,\
+//    `Is_Outside`, `Times`,`out_stamps`,`in_stamps`, `date`) \
+//   VALUES (?,?,?,?,?,?,?,?,current_timestamp())';
+
+//     const del_queryx = 'DELETE FROM `employee-status` WHERE barcode= (?) AND date =CURDATE()';
+
+//     // Check if employee Exist in database   
+//     con.query("SELECT * FROM `employee-status` WHERE barcode= (?) AND date =CURDATE()  ", [barcodeX], function (err, result, fields) {
+//       if (err) throw err;
+//       let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+//       resultx.forEach((v, k) => { console.log(v); });
+
+//       if (resultx.length == 1) {   //employee Exist in database 
+
+//         console.log('EMPLOYEE ALREADY EXIST :)')
+
+//         console.log(resultx.length);
+//         con.query(del_queryx,
+//           [barcodeX]
+//           , function (err, result, fields) {
+//             if (err) throw err;
+//             let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+//             //  resultx.forEach((v) => console.log(v));
+//           });
+//         console.log('EMPLOYEE DELETED :(')
+
+//         con.query(insert_queryx,
+//           [barcodeX, TimeStamp_OutX,
+//             TimeStamp_InX, TolatTimeSpentOutX,
+//             IsOutsideX, TimesX, out_stamps_x, in_stamps_x]
+//           , function (err, result, fields) {
+//             if (err) throw err;
+//             let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+//             // resultx.forEach((v) => console.log(v));
+//           });
+//         console.log('EMPLOYEE ADDED :)')
+//       }
+//       else {
+//         console.log('EMPLOYEE NOT EXIST :(')
+//         //Employee not exist
+//         // so insert new record "employee object"
+//         con.query(insert_queryx,
+//           [barcodeX, TimeStamp_OutX,
+//             TimeStamp_InX, TolatTimeSpentOutX,
+//             IsOutsideX, TimesX, out_stamps_x, in_stamps_x]
+//           , function (err, result, fields) {
+//             if (err) throw err;
+//             let resultx = Object.values(JSON.parse(JSON.stringify(result)));
+//             //resultx.forEach((v) => console.log(v));
+//           });
+//         console.log('EMPLOYEE ADDED :)')
+
+//       }
+//     });
 //   });
-// });
+// }
+
+
+
+//////////////////******* RETRIEVE DATA FROM DATABASE *******\\\\\\\\\\\\\\\\\\
+// function to retriver data from database in definite date..parissing it ..add it into object ..and store it in MAP..
+function getDataFromDB(date) {
+  var myMap = new Map();
+  con.connect(function (err) { // remove this please
+    if (err) throw err;
+    con.query("SELECT * FROM `employee-status`WHERE date=(?)  ", [date], function (err, result, fields) {
+      if (err) throw err;
+      console.log('result >>>>');
+      console.log(result);
+      for (let e in result) {
+        var x = new EmployeeStatus();
+        var resultX = JSON.stringify(result[e]);
+        var resultXObj = JSON.parse(resultX);
+         x.barcode=resultXObj.barcode;
+        x.TimeStamp_Out=resultXObj.Timestamp_Out
+        x.TimeStamp_In=resultXObj.Timestamp_In
+        x.TolatTimeSpentOut=resultXObj.Total_Time_Spent_Out
+        x.IsOutside=resultXObj.Is_Outside
+        x.Times=resultXObj.Times
+        x.out_stamps=resultXObj.out_stamps.split(",").map(Number); // convert to array  
+        x.in_stamps=resultXObj.in_stamps.split(",").map(Number);
+
+         myMap.set( x.barcode,x);
+                  }
+         console.log(myMap) 
+    });
+  });
+}
+//////////////////******* *** *** ** * ** *** *** *******\\\\\\\\\\\\\\\\\\
+
 
 //set_object(myemployee);
 
+//checkIfExist(myemployee);
 
-function checkIfExist(myemployee) {
-  // get the query result and print it 
-  //SElECT * FROM `employee-status` WHERE barcode = 'myemployee.barcode' AND date=CURDATE()
-  //if(myemployee.barcode==result){continue...}
-  // the select query must have barcode and currentdate conditions
-  // look at this SELECT count(*) FROM commands WHERE barcode = 'myemployee.barcode' FOR EXAMPLE
-  var x = new EmployeeStatus();
-  x = myemployee;
-
-  con.connect(function (err) {
-    if (err) throw err;
-    // work on this one
-    barcodeX = x.barcode;
-    IsOutsideX = x.IsOutside;
-    TimeStamp_OutX = x.TimeStamp_Out;
-    TimeStamp_InX = x.TimeStamp_In;
-    TimesX = x.Times;
-    TolatTimeSpentOutX = x.TolatTimeSpentOut;
-    const out_stamps_x = x.out_stamps.toString();
-    const in_stamps_x = x.in_stamps.toString();
-
-    const insert_queryx = 'INSERT INTO `employee-status` \
-  (`barcode`, `Timestamp Out`, `Timestamp In`, `Total Time Spent Out`,\
-   `Is Outside`, `Times`,`out stamps`,`in stamps`, `date`) \
-  VALUES (?,?,?,?,?,?,?,?,current_timestamp())';
-
-  const del_queryx = 'DELETE FROM `employee-status` WHERE barcode= (?) AND date =CURDATE()';
-
-    // Check if employee Exist in database   
-    con.query("SELECT * FROM `employee-status` WHERE barcode= (?) AND date =CURDATE()  ", [barcodeX], function (err, result, fields) {
-      if (err) throw err;
-      let resultx = Object.values(JSON.parse(JSON.stringify(result)));
-      resultx.forEach((v, k) => { console.log(v); });
-
-      if (resultx.length == 1) {   //employee Exist in database 
-
-        console.log('EMPLOYEE ALREADY EXIST :)')
-
-        console.log(resultx.length);
-        con.query(del_queryx,
-          [barcodeX]
-          , function (err, result, fields) {
-            if (err) throw err;
-             let resultx = Object.values(JSON.parse(JSON.stringify(result)));
-          //  resultx.forEach((v) => console.log(v));
-          });
-          console.log('EMPLOYEE DELETED :(')
-
-        con.query(insert_queryx,
-          [barcodeX, TimeStamp_OutX,
-            TimeStamp_InX, TolatTimeSpentOutX,
-            IsOutsideX, TimesX, out_stamps_x, in_stamps_x]
-          , function (err, result, fields) {
-            if (err) throw err;
-             let resultx = Object.values(JSON.parse(JSON.stringify(result)));
-           // resultx.forEach((v) => console.log(v));
-          });
-
-          console.log('EMPLOYEE ADDED :)')
+//getDataFromDB('2023-3-9')
 
 
-
-      }
-
-
-      else {
-        console.log('EMPLOYEE NOT EXIST :(')
-        //Employee not exist
-        // so insert new record "employee object 
-        con.query(insert_queryx,
-          [barcodeX, TimeStamp_OutX,
-            TimeStamp_InX, TolatTimeSpentOutX,
-            IsOutsideX, TimesX, out_stamps_x, in_stamps_x]
-          , function (err, result, fields) {
-            if (err) throw err;
-             let resultx = Object.values(JSON.parse(JSON.stringify(result)));
-            //resultx.forEach((v) => console.log(v));
-          });
-          console.log('EMPLOYEE ADDED :)')
-
-      }
-    });
-  });
-
-
-}
-
-
-
-
-checkIfExist(myemployee);
+insertEpmStatusInDB(temp_employee);
